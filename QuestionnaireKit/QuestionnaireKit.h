c2e496ee6fad2b3c26c9341fdaf2fded53fafea8
Dave Carlson <dcarlson@xmlmodeling.com>
2019-01-23 18:16:44 -0700

    Initial Commit

---
+++ QuestionnaireKit/QuestionnaireKit.h
@@ -0,0 +1,19 @@
+//
+//  QuestionnaireKit.h
+//  QuestionnaireKit
+//
+//  Created by Dave Carlson on 1/23/19.
+//  Copyright © 2019 Clinical Cloud Solutions, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuestionnaireKit.
+FOUNDATION_EXPORT double QuestionnaireKitVersionNumber;
+
+//! Project version string for QuestionnaireKit.
+FOUNDATION_EXPORT const unsigned char QuestionnaireKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuestionnaireKit/PublicHeader.h>
+
+
